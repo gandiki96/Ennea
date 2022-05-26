5948a1be2a5627e342dd39ffbaf45a1535bc047f
kiran gandi <gandiki96@gmsil.com>
2022-05-26 23:57:43 +0530

    projectCreation
    initial code commit

---
+++ EnneaSolutionsAssignment/src/main/java/org/ennea/EnneaSolutionsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package org.ennea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnneaSolutionsAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnneaSolutionsAssignmentApplication.class, args);
+	}
+
+}
